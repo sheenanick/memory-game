00d4464ca2cc028f0012fb48fda6fd3c640560da
Addison Nishijima and Sheena Nickerson <addison@>
2016-10-10 13:58:07 -0700

    separate backend from frontend

--- js/memory.js
+++ js/memory.js
@@ -1,3 +1,21 @@
-// function Turn() {
-//   this.cardsPicked = [];
-// }
+function Turn() {
+  this.cardsPicked = [];
+  this.cardIds = [];
+}
+
+Turn.prototype.flipCard = function(card, id) {
+  this.cardsPicked.push(card);
+  this.cardIds.push(id);
+  if (this.cardsPicked[0] == this.cardsPicked[1]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Turn.prototype.clear = function() {
+  this.cardsPicked = [];
+  this.cardIds = [];
+}
+
+exports.turnModule = Turn;
